7d3ad57067bf1aa6c8bc07f209224a4560c6d16a
BaeJungYeon <qowjdduswkd@naver.com>
2025-06-02 17:00:44 +0900

    MemberServiceTest 25.6.1

--- src/main/java/com/jungyeons/springintroductory/service/MemberService.java
+++ src/main/java/com/jungyeons/springintroductory/service/MemberService.java
@@ -4,11 +4,43 @@ import com.jungyeons.springintroductory.domain.Member;
 import com.jungyeons.springintroductory.repository.MemberRepository;
 import com.jungyeons.springintroductory.repository.MemoryMemberRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public class MemberService {
-    private MemberRepository memberRepository = new MemoryMemberRepository();
-    //회원 가입
-    public Long join(Member member) {
-    memberRepository.save(member);
-    return member.getId();
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public MemberService() {
     }
+
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
 }
--- src/test/java/com/jungyeons/springintroductory/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/com/jungyeons/springintroductory/repository/MemoryMemberRepositoryTest.java
@@ -1,56 +1,61 @@
 package com.jungyeons.springintroductory.repository;
-
 import com.jungyeons.springintroductory.domain.Member;
-import org.assertj.core.api.Assertions;
+import com.jungyeons.springintroductory.repository.MemoryMemberRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class MemoryMemberRepositoryTest {
-    MemberRepository repository = new MemoryMemberRepository();
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
 
     @AfterEach
     public void afterEach() {
-    repository.clearStore();
+        repository.clearStore();
     }
+
     @Test
-    public void save(){
+    public void save() {
         Member member = new Member();
-        member.setName("jungyeons");
+        member.setName("spring");
+
         repository.save(member);
+
         Member result = repository.findById(member.getId()).get();
         assertThat(member).isEqualTo(result);
     }
+
     @Test
-    public void findByName(){
+    public void findByName() {
         Member member1 = new Member();
-        member1.setName("jungyeons1");
+        member1.setName("spring1");
         repository.save(member1);
 
         Member member2 = new Member();
-        member2.setName("jungyeons2");
+        member2.setName("spring2");
         repository.save(member2);
 
-        Member result = repository.findByName("jungyeons1").get();
-        assertThat(member1).isEqualTo(result);
+        Member result = repository.findByName("spring1").get();
 
-
+        assertThat(result).isEqualTo(member1);
     }
-    @Test
-    public void findAll(){
 
+    @Test
+    public void findAll() {
         Member member1 = new Member();
-        member1.setName("jungyeons1");
+        member1.setName("spring1");
         repository.save(member1);
 
         Member member2 = new Member();
-        member2.setName("jungyeons2");
+        member2.setName("spring2");
         repository.save(member2);
+
         List<Member> result = repository.findAll();
+
         assertThat(result.size()).isEqualTo(2);
     }
+
 }
