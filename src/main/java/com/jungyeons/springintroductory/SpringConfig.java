26bf14738b4b61c3755169c473f12279493417fb
BaeJungYeon <qowjdduswkd@naver.com>
2025-06-04 20:48:00 +0900

    SpringJDBC 25.6.4

--- src/main/java/com/jungyeons/springintroductory/SpringConfig.java
+++ src/main/java/com/jungyeons/springintroductory/SpringConfig.java
@@ -1,30 +1,35 @@
 package com.jungyeons.springintroductory;
 
+import com.jungyeons.springintroductory.repository.JdbcTemplateMemberRepository;
 import com.jungyeons.springintroductory.repository.MemberRepository;
+import com.jungyeons.springintroductory.repository.MemoryMemberRepository;
 import com.jungyeons.springintroductory.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
 
-    private final MemberRepository memberRepository;
+ //   private final MemberRepository memberRepository;
+    private DataSource dataSource;
 
-    public SpringConfig(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository);
-    }
+//    public SpringConfig(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
 
 //    @Bean
-//    public MemberRepository memberRepository() {
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository);
+//    }
+
+    @Bean
+    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
 //        return new JpaMemberRepository(em);
-//    }
+    }
 
 }
